7f6062859fe071b9b74eb8fee9252e430980fa15
Alois Carrera <aloiscarrera@gmail.com>
2020-12-05 22:06:50 -0500

    refactor: :art: Add absolute conditional style to svg

--- src/components/landing/atoms/CybersecurityLogo.js
+++ src/components/landing/atoms/CybersecurityLogo.js
@@ -1,16 +1,28 @@
 import React from "react"
+import PropTypes from "prop-types"
 import tw from "twin.macro"
-
 import CybersecurityLogoSVG from "../../../images/cyber-security.svg"
 
-const StyledCybersecurityIcon = tw.div`absolute text-white top-8 left-8`
+const CybersecurityLogo = ({ isAbsolute }) => {
+  const StyledCybersecurityIcon = isAbsolute
+    ? tw.div`absolute text-white top-8 left-8`
+    : tw.div`text-white top-8 left-8`
 
-const CybersecurityLogo = () => (
-  <StyledCybersecurityIcon>
-    <CybersecurityLogoSVG
-      style={{ maxWidth: `60px`, maxHeight: `60px`, fill: `currentColor` }}
-    />
-  </StyledCybersecurityIcon>
-)
+  return (
+    <StyledCybersecurityIcon>
+      <CybersecurityLogoSVG
+        style={{ maxWidth: `60px`, maxHeight: `60px`, fill: `currentColor` }}
+      />
+    </StyledCybersecurityIcon>
+  )
+}
+
+CybersecurityLogo.propTypes = {
+  isAbsolute: PropTypes.bool,
+}
+
+CybersecurityLogo.defaultProps = {
+  isAbsolute: false,
+}
 
 export default CybersecurityLogo
