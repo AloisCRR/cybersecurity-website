6abed47e8e153cc817cfd37416ae675c028647fb
Alois Carrera <aloiscarrera@gmail.com>
2020-12-06 09:17:49 -0500

    refactor: :art: Change some styles and wrong HTML tags

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import tw from "twin.macro"
 
-const StyledBase = tw.footer`font-cyber w-full py-3.5 bg-gray-800 text-white flex justify-center items-center`
+const StyledBase = tw.footer`font-cyber w-full py-3.5 bg-black text-white flex justify-center items-center border-t-4 border-gray-800`
 
 const Footer = () => (
   <StyledBase>
--- src/components/landing/atoms/AboutInfo.js
+++ src/components/landing/atoms/AboutInfo.js
@@ -2,18 +2,17 @@ import React from "react"
 import tw from "twin.macro"
 
 const StyledAboutInfo = tw.div`bg-info p-10 flex-1`
-const StyledAboudInfoTitleText = tw.p`uppercase font-bold text-2xl tracking-wider`
-const StyledAboudInfoContentText = tw.p``
+const StyledAboutInfoTitleText = tw.p`uppercase font-bold text-2xl tracking-wider`
 const LineSeparator = tw.hr`my-10 w-1/3`
 const StyledParagraph = tw.p`mb-6 text-justify`
 
 const AboutInfo = () => (
   <StyledAboutInfo>
-    <StyledAboudInfoTitleText>
+    <StyledAboutInfoTitleText>
       ¿De qué se trata la ciberseguridad?
-    </StyledAboudInfoTitleText>
+    </StyledAboutInfoTitleText>
     <LineSeparator />
-    <StyledAboudInfoContentText>
+    <div>
       <StyledParagraph>
         Un enfoque de seguridad cibernética exitoso tiene múltiples capas de
         protección repartidas por las computadoras, redes, programas o datos que
@@ -30,7 +29,7 @@ const AboutInfo = () => (
         clave de las operaciones de seguridad: detección, investigación y
         reparación.
       </p>
-    </StyledAboudInfoContentText>
+    </div>
   </StyledAboutInfo>
 )
 
